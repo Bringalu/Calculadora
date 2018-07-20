7a79d86bc78fdd0b17a0e3c84187f163987ec5f8
Bringalu <bringhentilucca@gmail.com>
2018-07-19 23:28:41 -0300

    V.2
    Versão um pouquinho mais avançada de uma calculadora básica

--- Calculadora1.0/README.md
+++
@@ -1,3 +0,0 @@
-# Calculadora Básica em Java
-# Feita por: Lucca Bringhenti
-## Java 8 (JRE8.0) no Eclipse Photon
---
+++ Calculadora2.0/src/main/Mats.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Mats {
+
+	public int mas(int a, int b) {
+		return a+b;
+	}
+	public int men(int a, int b) {
+		return a-b;
+	}
+	public int vez(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b) {
+		return a/b;
+	}
+	
+}
